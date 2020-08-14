1fb46ddb2199ac1aab30c453544f799effa9087a
Dmitry Polyakovsky <dmitry.polyakovsky@oracle.com>
2020-08-14 16:05:12 -0700

    added docker and locust files

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# locust1
+# With Docker in distributed mode
+
+```
+docker-compose up --build -d --scale worker=5
+```
+
+# Without Docker in not distributed mode
+
+```
+# run Redis server
+# run Python
+pipenv install --dev
+pipenv shell
+locust --config config/locust.conf
+```
---
+++ locustfile.py
@@ -0,0 +1,37 @@
+import os
+import uuid
+import random
+import logging
+from locust import HttpUser, User, task, between
+import redis
+
+
+class WebUser(HttpUser):
+    weight = 2
+    wait_time = between(5, 9)
+    host = 'http://master:8089'
+
+    @task
+    def index_page(self):
+        resp = self.client.get('/')
+        logging.info(resp)
+
+
+class RedisUser(User):
+    weight = 3
+    wait_time = between(1, 3)
+
+    @task
+    def set(self):
+        key = str(uuid.uuid4())
+        value = str(uuid.uuid4())
+        resp = self.rc.set(key, value)
+        logging.info(resp)
+
+    def on_start(self):
+        self.rc = redis.StrictRedis(host=os.environ.get('REDIS_HOST'), charset='utf-8', decode_responses=True)
+        logging.info('on_start')
+
+    def on_stop(self):
+        self.rc.flushdb()
+        logging.info('on_stop')
